eaeb337c1c50d24fd4c04fd61c4a3fc558f2a04c
WahidinAji <a17wahidin@gmail.com>
2020-09-03 21:16:55 +0700

    Setup awal and Migration, Model, PlayerAuthController, and add Player in config/auth

---
+++ app/Http/Controllers/Player/PlayerAuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Player;
+
+use App\Http\Controllers\Controller;
+use App\Model\Player;
+use Illuminate\Http\Request;
+use Validator, Redirect, Response;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Session;
+
+class PlayerAuthController extends Controller
+{
+    
+}
---
+++ app/Model/Player.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Model;
+
+// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Player extends Authenticatable
+{
+    use Notifiable;
+    protected $fillable = [
+        'name', 'email', 'password', 'address', 'contact', 'ava_url'
+    ];
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    public $timestamps = \true;
+}
